43826c4abb1e836616feaaf424e8631baaff9923
Pedro Ramos <pedrowarkenramos@gmail.com>
2025-02-07 17:20:13 -0300

    feat: migrations updates

--- infra/database.js
+++ infra/database.js
@@ -1,16 +1,9 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
-    user: process.env.POSTGRES_USER,
-    host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    port: process.env.POSTGRES_PORT,
-    ssl: process.env.NODE_ENV === "production" ? true : false,
-  });
-  await client.connect();
+  let client;
   try {
+    client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
   } finally {
@@ -37,9 +30,27 @@ async function getPostgresVersion() {
   return result.rows[0].server_version;
 }
 
+function getSSLValues() {
+  return process.env.NODE_ENV === "production" ? true : false;
+}
+
+async function getNewClient() {
+  const client = new Client({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
+    ssl: getSSLValues(),
+  });
+  await client.connect();
+  return client;
+}
+
 export default {
   query,
   getMaxConnections,
   getCurrentConnections,
   getPostgresVersion,
+  getNewClient,
 };
--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,30 +1,32 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
+import database from "infra/database";
 
 export default async function migrations(request, response) {
-  let migrations;
-  switch (request.method) {
-    case "GET":
-      migrations = await migrationRunner({
-        databaseUrl: process.env.DATABASE_URL,
-        dryRun: true,
-        dir: join("infra", "migrations"),
-        direction: "up",
-        verbose: true,
-        migrationsTable: "pgmigrations",
-      });
-      response.status(200).json(migrations);
-      break;
-    case "POST":
-      migrations = await migrationRunner({
-        databaseUrl: process.env.DATABASE_URL,
-        dryRun: false,
-        dir: join("infra", "migrations"),
-        direction: "up",
-        verbose: true,
-        migrationsTable: "pgmigrations",
-      });
-      response.status(200).json(migrations);
-      break;
+  const dbClient = await database.getNewClient();
+  const defaultMigrationOptions = {
+    dbClient,
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+    await dbClient.end();
+    return response.status(200).json(pendingMigrations);
+  }
+  if (request.method === "POST") {
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+    });
+    await dbClient.end();
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+    return response.status(200).json(migratedMigrations);
   }
 }
