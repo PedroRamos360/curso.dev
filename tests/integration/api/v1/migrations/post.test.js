a7b7dd9291157124f442900a81a7895551512280
Pedro Ramos <pedrowarkenramos@gmail.com>
2025-02-13 10:00:25 -0300

    feat: wait for next to be up to run tests

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,10 +1,10 @@
 import database from "infra/database.js";
+import orchestrator from "tests/orchestrator.js";
 
-beforeAll(cleanDatabase);
-
-async function cleanDatabase() {
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
   await database.query("drop schema public cascade; create schema public;");
-}
+});
 
 test("POST /api/v1/migrations should return 200", async () => {
   const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
@@ -13,14 +13,12 @@ test("POST /api/v1/migrations should return 200", async () => {
   expect(response1.status).toBe(201);
   const response1Body = await response1.json();
   expect(Array.isArray(response1Body)).toBe(true);
-  console.log(response1Body);
   expect(response1Body.length).toBeGreaterThan(0);
   const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
   });
   expect(response2.status).toBe(200);
   const response2Body = await response2.json();
-  console.log(response2Body);
   expect(Array.isArray(response2Body)).toBe(true);
   expect(response2Body.length).toBe(0);
 });
